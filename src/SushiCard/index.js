13deea4273cb194db2a0a819029ec5d69927d873
nesclapez@gmail.com <nesclapez@gmail.com>
2020-09-29 11:45:43 +0200

    feacture add use counter

--- src/SushiCard/index.js
+++ src/SushiCard/index.js
@@ -10,18 +10,29 @@ import {
   Typography,
   IconButton,
 } from "@material-ui/core";
-import { AddBox, IndeterminateCheckBox } from "@material-ui/icons";
+import {
+  AddBox,
+  IndeterminateCheckBox,
+  InsertChartRounded,
+} from "@material-ui/icons";
 
 import { string } from "prop-types";
 
 import useStyles from "./styles";
 
+//import useCounter from "../useCounter";
+
+import { useCounter } from "react-use";
+
 export default function SushiCard({ image, title, description }) {
   const classes = useStyles();
 
+  //const [quantity, increment, decrement] = useCounter(1);
+  const [quantity, { inc, dec }] = useCounter(1, 10, 1);
+
   return (
     <Card className={classes.root}>
-      <CardMedia className={classes.media} image={image} title="Paella dish" />
+      <CardMedia className={classes.media} image={image} title={title} />
       <CardContent>
         <Typography gutterBottom variant="h5" component="h2">
           {title}
@@ -29,11 +40,15 @@ export default function SushiCard({ image, title, description }) {
         <Typography variant="body2" color="textSecondary" component="p">
           {description}
         </Typography>
-        <IconButton aria-label="delete" className={classes.margin} size="small">
+        <IconButton aria-label="ajouter" onClick={() => inc()}>
           <AddBox fontSize="inherit" />
         </IconButton>
-        <Input name="quantity" inputProps={{ "aria-label": "quantity" }} />
-        <IconButton aria-label="delete" className={classes.margin} size="small">
+        <Input
+          name="quantity"
+          value={quantity}
+          inputProps={{ "aria-label": "quantity" }}
+        />
+        <IconButton aria-label="supprimer" onClick={() => dec()}>
           <IndeterminateCheckBox fontSize="inherit" />
         </IconButton>
       </CardContent>

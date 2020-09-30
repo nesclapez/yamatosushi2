077aa2ade569c9061547d85ca909ebf970ed990a
nesclapez@gmail.com <nesclapez@gmail.com>
2020-09-30 15:06:07 +0200

    Basket screen hard mode :)

---
+++ src/BasketItemList/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import IconButton from "@material-ui/core/IconButton";
+import Delete from "@material-ui/icons/Delete";
+
+import { arrayOf, number, shape, string, func } from "prop-types";
+
+export default function BasketItemList({ sushis, removeAt }) {
+  return (
+    <List component="ol" aria-label="basket item list">
+      {sushis.map(({ id, title }, i) => (
+        <ListItem key={id}>
+          <ListItemText primary={title} />
+          <ListItemSecondaryAction>
+            <IconButton
+              edge="end"
+              arial-label="delete"
+              onClick={() => {
+                removeAt(i);
+              }}
+            >
+              <Delete />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+      ))}
+    </List>
+  );
+}
+
+BasketItemList.propTypes = {
+  sushis: arrayOf(
+    shape({
+      id: Number,
+      title: string,
+    })
+  ),
+  removeAt: func,
+};
--- src/BasketScreen/index.js
+++ src/BasketScreen/index.js
@@ -1,6 +1,28 @@
 import React from "react";
+import { Button } from "@material-ui/core";
+import { useList } from "react-use";
+
 import Screen from "../Screen";
+import BasketItemList from "../BasketItemList";
 
 export default function BasketScreen() {
-  return <Screen title="Basket" />;
+  const [sushis, { removeAt, clear }] = useList([
+    { id: 1, title: "Sushi 1" },
+    { id: 2, title: "Sushi 2" },
+    { id: 3, title: "Sushi 3" },
+    { id: 4, title: "Sushi 4" },
+    { id: 5, title: "Sushi 5" },
+    { id: 6, title: "Sushi 6" },
+  ]);
+
+  const buy = () => clear();
+
+  return (
+    <Screen title="Basket">
+      <BasketItemList sushis={sushis} removeAt={removeAt} />
+      <Button variant="contained" color="primary" onClick={buy}>
+        Primary
+      </Button>
+    </Screen>
+  );
 }
